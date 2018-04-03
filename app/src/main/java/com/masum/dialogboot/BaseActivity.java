b6e0bf1e4cdf29a05b35d7493c4c3ee1512c9e45
Masum <mnhmasum@gmail.com>
2018-04-04 00:30:08 +0600

    Add maven local to the root gradle

--- app/src/main/java/com/masum/dialogboot/BaseActivity.java
+++ app/src/main/java/com/masum/dialogboot/BaseActivity.java
@@ -13,6 +13,6 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DialogBootLoader.bind(this);
+        //DialogBootLoader.bind(this);
     }
 }
